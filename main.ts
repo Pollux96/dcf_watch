ed688376ec775c6fe7c9bd5ce60abfdb1b51aa66
Pollux96 <110056689+Pollux96@users.noreply.github.com>
2023-06-08 17:36:37 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,33 @@
+function Zeige_Uhrzeit () {
+    strip.setPixelColor(Stunde * 2 / 24 + 12, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(Minute / 2.5 + 12, neopixel.colors(NeoPixelColors.Green))
+    strip.setPixelColor(Sekunde / 2.5 + 12, neopixel.colors(NeoPixelColors.Blue))
+    strip.show()
+}
+function Berechne_Uhrzeit () {
+    Sekunde = Sekunde + 1
+    if (Sekunde > 59) {
+        Minute = Minute + 1
+        Sekunde = 0
+    }
+    if (Minute > 59) {
+        Stunde = Stunde + 1
+        Minute = 0
+    }
+    if (Stunde > 23) {
+        Stunde = 0
+    }
+}
+let Sekunde = 0
+let Minute = 0
+let Stunde = 0
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P12, 24, NeoPixelMode.RGB)
+Stunde = 0
+Minute = 0
+Sekunde = 0
+basic.forever(function () {
+    basic.pause(1000)
+    Berechne_Uhrzeit()
+    Zeige_Uhrzeit()
+})
