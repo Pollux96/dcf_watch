3f2c64710cbf9e154ec29d7c2245899496443e85
Pollux96 <110056689+Pollux96@users.noreply.github.com>
2023-07-03 18:25:10 +0200

    Löschen der alten Pixel umgesetzt

--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
 function Zeige_Uhrzeit () {
-    strip.setPixelColor(Stunde * 2 / 24 + 12, neopixel.colors(NeoPixelColors.Red))
-    strip.setPixelColor(Minute / 2.5 + 12, neopixel.colors(NeoPixelColors.Green))
-    strip.setPixelColor(Sekunde / 2.5 + 12, neopixel.colors(NeoPixelColors.Blue))
+    strip.setPixelColor(PixelStunde, neopixel.colors(NeoPixelColors.Black))
+    PixelStunde = Stunde * 2 / 24
+    strip.setPixelColor(PixelStunde, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(PixelMinute, neopixel.colors(NeoPixelColors.Black))
+    PixelMinute = Minute / 2.5
+    strip.setPixelColor(PixelMinute, neopixel.colors(NeoPixelColors.Green))
+    strip.setPixelColor(PixelSekunde, neopixel.colors(NeoPixelColors.Black))
+    PixelSekunde = Sekunde / 2.5
+    strip.setPixelColor(PixelSekunde, neopixel.colors(NeoPixelColors.Blue))
     strip.show()
 }
 function Berechne_Am_Pm () {
@@ -25,17 +31,31 @@ function Berechne_Uhrzeit () {
         Stunde = 0
     }
 }
+let PixelSekunde = 0
+let PixelMinute = 0
+let PixelStunde = 0
 let Sekunde = 0
 let Minute = 0
 let Stunde = 0
 let strip: neopixel.Strip = null
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . # . .
+    # . . . #
+    . # # # .
+    `)
+basic.pause(2000)
 strip = neopixel.create(DigitalPin.P12, 24, NeoPixelMode.RGB)
 Stunde = 0
 Minute = 0
 Sekunde = 0
 basic.forever(function () {
-    basic.pause(1000)
+    basic.pause(2000)
+    Berechne_Am_Pm()
+})
+basic.forever(function () {
+    basic.pause(100)
     Berechne_Uhrzeit()
     Zeige_Uhrzeit()
-    Berechne_Am_Pm()
 })
